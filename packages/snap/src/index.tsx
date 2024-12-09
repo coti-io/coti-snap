abf0e0411a72e9f4a39485fcc63ba8415bdf6747
Evgeny Nasretdinov <evgeny.nasretdinov@gmail.com>
2024-12-10 00:08:58 +0100

    import tokens and recalculate balances

--- packages/snap/src/index.tsx
+++ packages/snap/src/index.tsx
@@ -34,17 +34,70 @@ const tokens = [
   },
   {
     name: 'TEST',
-    symbol: 'PRT',
+    symbol: 'TST',
     address: '0x0a4db7035284566f6f676991ed418140dc01a2aa',
   }
 ]
 
+type tokensType = { name: string, symbol: string, address: string, balance: string | null }[];
+
 // should be stored in a secure storage after onboarding process
-const testAESKey = '';
+const testAESKey = '50764f856be3f636c09faf092be20d0c';
+
+export const recalculateBalances = async () => {
+  // TODO: tokens =  get token balances from state
+  const state = await snap.request({
+    method: "snap_manageState",
+    params: { operation: "get" },
+  })
+
+  const tokens = state?.tokenBalances ? JSON.parse(state?.tokenBalances as string) as tokensType : [];
+
+  const provider = new BrowserProvider(ethereum);
+  const signer = await provider.getSigner();
+  const signerAddress = await signer.getAddress();
+  const balance = await provider.getBalance(signerAddress);
+
+  const tokenBalances = await Promise.all(tokens.map(async (token) => {
+    const tokenContract = new Contract(token.address, erc20Abi, signer);
+    const tok = (tokenContract.connect(signer) as Contract)
+    const tokenBalance = tok.balanceOf ? await tok.balanceOf() : null;
+    const decryptedBalance = tokenBalance ? decryptUint(tokenBalance, testAESKey) : null;
+    return { ...token, balance: decryptedBalance?.toString() || null };
+  }));
+
+  await snap.request({
+    method: "snap_manageState",
+    params: {
+      operation: "update",
+      newState: { balance: String(balance), tokenBalances: JSON.stringify(tokenBalances) },
+    },
+  })
+
+  return { balance, tokenBalances };
+}
+
+export const importToken = async (address: string, name: string, symbol: string) => {
+  const oldState = await snap.request({
+    method: "snap_manageState",
+    params: { operation: "get" },
+  });
+
+  const tokens = JSON.parse(oldState?.tokenBalances as string) as tokensType;
+  tokens.push({ address, name, symbol, balance: null });
+
+  await snap.request({
+    method: "snap_manageState",
+    params: {
+      operation: "update",
+      newState: { ...oldState, tokenBalances: JSON.stringify(tokens) },
+    },
+  })
+}
 
 export const Home = ({balance, tokenBalances} :{
   balance: bigint,
-  tokenBalances: { name: string, balance: bigint | null, symbol: string, address: string }[]
+  tokenBalances: tokensType
 }) => {
   return (
     <Box>
@@ -63,21 +116,7 @@ export const Home = ({balance, tokenBalances} :{
 };
 
 export const onHomePage: OnHomePageHandler = async () => {
-
-  // TODO: move this calculation to a helper function and get tokens from state
-  const provider = new BrowserProvider(ethereum);
-  const signer = await provider.getSigner();
-  const signerAddress = await signer.getAddress();
-  const balance = await provider.getBalance(signerAddress);
-
-  const tokenBalances = await Promise.all(tokens.map(async (token) => {
-    const tokenContract = new Contract(token.address, erc20Abi, signer);
-    const tok = (tokenContract.connect(signer) as Contract)
-    const tokenBalance = tok.balanceOf ? await tok.balanceOf() : null;
-    const decryptedBalance = tokenBalance ? decryptUint(tokenBalance, testAESKey) : null;
-    return { ...token, balance: decryptedBalance };
-  }));
-
+  const {balance, tokenBalances} = await recalculateBalances();
   return { content: <Home balance={balance} tokenBalances={tokenBalances} /> };
 };
 
@@ -114,22 +153,46 @@ export const onUserInput: OnUserInputHandler = async ({ id, event }) => {
         });
         break;
       case 'token-cancel':
-        // TODO: get balances from state
+        const persistedData = await snap.request({
+          method: "snap_manageState",
+          params: { operation: "get" },
+        })
+        const stateBalance = BigInt((persistedData?.balance as string) || 0);
+        const stateTokenBalances = JSON.parse((persistedData?.tokenBalances as string) || "[]");
         await snap.request({
           method: "snap_updateInterface",
           params: {
             id,
-            ui: <Home balance={0n} tokenBalances={[]} />,
+            ui: <Home balance={stateBalance} tokenBalances={stateTokenBalances} />,
           },
         });
         break;
       case 'token-submit':
-        // TODO: recalculate token balances
+        // import token
+        const state = await snap.request({
+          method: "snap_getInterfaceState",
+          params: {
+            id,
+          },
+        })
+
+        const formState = state['form-to-fill'] as Record<string, string | boolean | null>;
+        if (formState && formState['token-address'] && formState['token-name']) {
+          const address = formState['token-address'] as string;
+          const name = formState['token-name'] as string;
+          const symbol = name.slice(0, 3).toUpperCase();
+          await importToken(address, name, symbol);
+        } else {
+          //add validation
+          console.log('Invalid form state:', formState);
+        }
+        
+        const {balance, tokenBalances} = await recalculateBalances();
         await snap.request({
           method: "snap_updateInterface",
           params: {
             id,
-            ui: <Home balance={0n} tokenBalances={[]} />,
+            ui: <Home balance={balance} tokenBalances={tokenBalances} />,
           },
         });
         break;
