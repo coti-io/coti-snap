058696e66e2749b8c1d3e1428bd324baa75e581b
geraBarboni <gerardobarboni@gmail.com>
2024-12-30 07:23:38 -0300

    import token

--- packages/snap/src/components/ImportToken.tsx
+++ packages/snap/src/components/ImportToken.tsx
@@ -1,23 +1,42 @@
-import { Container, Form, Button, Input, Field, Footer } from '@metamask/snaps-sdk/jsx';
+import {
+  Container,
+  Form,
+  Button,
+  Input,
+  Field,
+  Footer,
+} from '@metamask/snaps-sdk/jsx';
 
 export const ImportToken = () => {
   return (
     <Container>
       <Form name="form-to-fill">
-        <Field label="Token Name" error='Token name is required'>
+        <Field label="Token Name" error="Token name is required">
           <Input name="token-name" placeholder="Token Name" />
         </Field>
-        <Field label="Token Symbol" error='Token symbol is required'>
+        <Field label="Token Symbol" error="Token symbol is required">
           <Input name="token-symbol" placeholder="Token Symbol" />
         </Field>
-        <Field label="Token Address" error='Token address is required'>
+        <Field label="Token Address" error="Token address is required">
           <Input name="token-address" placeholder="0x123..." />
         </Field>
+        {/*
+          //TODO: Add token icon
+          <Field label="Token Icon">
+            <Input name="token-image" placeholder="https//..." />
+          </Field>
+          //TODO: Add token decimals
+          <Field label="Token Decimals">
+            <Input name="token-decimals" placeholder="8" />
+          </Field>
+          */}
       </Form>
       <Footer>
-        <Button name="token-submit" type='submit'>Submit</Button>
         <Button name="token-cancel">Cancel</Button>
+        <Button name="token-submit" type="submit">
+          Submit
+        </Button>
       </Footer>
     </Container>
   );
-}
+};
