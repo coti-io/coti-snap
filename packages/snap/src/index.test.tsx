9db8fbb0d0f2b7475ceeab160f00a51a3e1f3245
Evgeny Nasretdinov <evgeny.nasretdinov@gmail.com>
2025-01-22 16:03:14 +0100

    snap's rpc calls test

--- packages/snap/src/index.test.tsx
+++ packages/snap/src/index.test.tsx
@@ -1,45 +1,145 @@
+import { decrypt, encodeKey, encodeString, encrypt } from '@coti-io/coti-sdk-typescript';
 import { expect } from '@jest/globals';
-import type { SnapConfirmationInterface } from '@metamask/snaps-jest';
+import { error } from '@metamask/snaps-cli/dist/utils/logging.cjs';
 import { installSnap } from '@metamask/snaps-jest';
-import { Box, Text, Bold } from '@metamask/snaps-sdk/jsx';
+import { Box, Text, Heading } from '@metamask/snaps-sdk/jsx';
+import type { RequestOptions, SnapConfirmationInterface, SnapRequest } from "@metamask/snaps-simulation";
 
 describe('onRpcRequest', () => {
-  describe('hello', () => {
-    it('shows a confirmation dialog', async () => {
-      const { request } = await installSnap();
+  it('handles encryption with a valid AES key', async () => {
+    const { request } = await installSnap();
+    const aesKey = 'test-aes-key';
 
-      const origin = 'Jest';
-      const response = request({
-        method: 'hello',
-        origin,
-      });
-
-      const ui = (await response.getInterface()) as SnapConfirmationInterface;
-      expect(ui.type).toBe('confirmation');
-      expect(ui).toRender(
-        <Box>
-          <Text>
-            Hello, <Bold>{origin}</Bold>!
-          </Text>
-          <Text>This custom confirmation is just for display purposes.</Text>
-          <Text>
-            But you can edit the snap source code to make it do something, if
-            you want to!
-          </Text>
-        </Box>,
-      );
-
-      await ui.ok();
-
-      expect(await response).toRespondWith(true);
+    await request({
+      method: 'set-aes-key',
+      params: { newUserAesKey: aesKey },
     });
+
+    const textToEncrypt = 'Hello, encrypt!';
+    const response = request({
+      method: 'encrypt',
+      params: { value: textToEncrypt },
+    });
+
+    const ui = (await response.getInterface()) as SnapConfirmationInterface;
+    expect(ui.type).toBe('confirmation');
+    expect(ui).toRender(
+      <Box>
+        <Heading>Would you like to encrypt this value?</Heading>
+        <Text>Value to encrypt: "{textToEncrypt}"</Text>
+      </Box>,
+    );
+
+    await ui.ok();
+    const rpcResponse = await response as { response: { result: string } };
+    expect(rpcResponse).toRespondWith(expect.any(String));
+    expect(rpcResponse.response.result).toBeDefined();
+
+    const result = rpcResponse.response.result;
+    const r = JSON.parse(result).r as Object
+    const ciphertext = JSON.parse(result).ciphertext as Object
+    const decryptResult = decrypt(encodeKey(aesKey), new Uint8Array([...Object.values(r)]), new Uint8Array([...Object.values(ciphertext)]))
+    const decoder = new TextDecoder('utf-8');
+    const decodedString = decoder.decode(decryptResult).replace(/^\u0000+/, '');
+
+    expect(decodedString).toBe(textToEncrypt);
   });
 
-  it('throws an error if the requested method does not exist', async () => {
+  it('handles decryption with a valid AES key', async () => {
+    const { request } = await installSnap();
+    const aesKey = 'test-aes-key';
+
+    // encrypted string of "Hello, encrypt!"
+    const encryptedValue = `{"ciphertext":{"0":200,"1":164,"2":9,"3":14,"4":128,"5":3,"6":140,"7":76,"8":179,"9":61,"10":14,"11":109,"12":166,"13":242,"14":167,"15":210},"r":{"0":255,"1":18,"2":79,"3":60,"4":244,"5":42,"6":201,"7":192,"8":146,"9":103,"10":201,"11":91,"12":51,"13":91,"14":169,"15":84}}`;
+
+    // Ensure the AES key is set
+    await request({
+      method: 'set-aes-key',
+      params: { newUserAesKey: aesKey },
+    });
+
+    const response = request({
+      method: 'decrypt',
+      params: { value: encryptedValue },
+    });
+
+    const ui = (await response.getInterface()) as SnapConfirmationInterface;
+    expect(ui.type).toBe('confirmation');
+    expect(ui).toRender(
+      <Box>
+        <Heading>Would you like to decrypt this value?</Heading>
+        <Text>Value to decrypt: "{encryptedValue}"</Text>
+      </Box>,
+    );
+
+    await ui.ok();
+    const rpcResponse = await response;
+    expect(rpcResponse).toBeDefined();
+
+    const result = (rpcResponse.response as { result: string }).result;
+    const decryptResult = new Uint8Array([...Object.values(JSON.parse(result) as Object)]);
+    const decoder = new TextDecoder('utf-8');
+    const decodedString = decoder.decode(decryptResult).replace(/^\u0000+/, '');
+    expect(decodedString).toBe('Hello, encrypt!');
+  });
+
+  it('throws an error if AES key is not set for encryption', async () => {
+    const { request } = await installSnap();
+    const textToEncrypt = 'Hello, world!';
+
+    const response = request({
+      method: 'encrypt',
+      params: { value: textToEncrypt },
+    });
+
+    const ui = (await response.getInterface()) as SnapConfirmationInterface;
+    expect(ui.type).toBe('alert');
+    expect(ui).toRender(
+      <Box>
+        <Heading>Warning</Heading>
+        <Text>Cannot encrypt value. AES key not found.</Text>
+      </Box>,
+    );
+
+    await ui.ok();
+    const rpcResponse = await response;
+    const result = (rpcResponse.response as { result: null }).result;
+    expect(result).toBeNull();
+  });
+
+  it('handles fetching the AES key', async () => {
+    const { request } = await installSnap();
+    const aesKey = 'test-aes-key';
+
+    await request({
+      method: 'set-aes-key',
+      params: { newUserAesKey: aesKey },
+    });
+
+    const response = request({
+      method: 'get-aes-key',
+    });
+
+    const ui = (await response.getInterface()) as SnapConfirmationInterface;
+    expect(ui.type).toBe('confirmation');
+    expect(ui).toRender(
+      <Box>
+        <Heading>Reveal AES Key</Heading>
+        <Text>Approve to reveal AES Key</Text>
+      </Box>,
+    );
+
+    await ui.ok();
+    const rpcResponse = await response;
+    const result = (rpcResponse.response as { result: string }).result;
+    expect(result).toBe(aesKey);
+  });
+
+  it('throws an error for unknown methods', async () => {
     const { request } = await installSnap();
 
     const response = await request({
-      method: 'foo',
+      method: 'unknown-method',
     });
 
     expect(response).toRespondWithError({
