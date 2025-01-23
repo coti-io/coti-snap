7c4f9851842bb7e624f666df7eacaa733c32cbab
geraBarboni <gerardobarboni@gmail.com>
2025-01-23 20:19:01 -0300

    tesnet config

--- packages/site/src/config/wagmi.ts
+++ packages/site/src/config/wagmi.ts
@@ -1,26 +1,26 @@
 import { defineChain } from 'viem';
 import { http, createConfig } from 'wagmi';
 
-export const CHAIN_ID = 15151515;
+export const CHAIN_ID = 7082400;
 
 export const COTI = defineChain({
   id: CHAIN_ID,
-  name: 'COTID',
+  name: 'COTI',
   caipNetworkId: 'eip155:123456789',
   chainNamespace: 'eip155',
   nativeCurrency: {
-    decimals: 6,
-    name: 'COTID',
-    symbol: 'COTID',
+    decimals: 18,
+    name: 'COTI',
+    symbol: 'COTI',
   },
   rpcUrls: {
     default: {
-      http: ['http://35.159.136.210:7002'],
-      webSocket: ['WS_RPC_URLwss://devnet.coti.io/ws'],
+      http: ['https://testnet.coti.io/rpc'],
+      webSocket: ['WS_RPC_URLwss://testnet.coti.io/ws'],
     },
   },
   blockExplorers: {
-    default: { name: 'Explorer', url: 'https://devnet.cotiscan.io' },
+    default: { name: 'Explorer', url: 'https://testnet.cotiscan.io' },
   },
   contracts: {},
 });
@@ -31,34 +31,3 @@ export const config = createConfig({
     [COTI.id]: http(),
   },
 });
-
-// export const CHAIN_ID = 13068200;
-
-// export const COTI = defineChain({
-//   id: CHAIN_ID,
-//   name: 'COTI',
-//   caipNetworkId: 'eip155:123456789',
-//   chainNamespace: 'eip155',
-//   nativeCurrency: {
-//     decimals: 18,
-//     name: 'COTI 2',
-//     symbol: 'COTI2',
-//   },
-//   rpcUrls: {
-//     default: {
-//       http: ['https://devnet.coti.io/rpc'],
-//       webSocket: ['WS_RPC_URLwss://devnet.coti.io/ws'],
-//     },
-//   },
-//   blockExplorers: {
-//     default: { name: 'Explorer', url: 'https://devnet.cotiscan.io' },
-//   },
-//   contracts: {},
-// });
-
-// export const config = createConfig({
-//   chains: [COTI],
-//   transports: {
-//     [COTI.id]: http(),
-//   },
-// });
