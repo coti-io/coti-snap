2d35ee88f1a41b3c6d27c00d7eff334d1a422ae2
Rafa <86583080+RafaBlockDev@users.noreply.github.com>
2025-07-14 11:15:01 -0600

    feat: let wrong chain button switch chain (#10)

--- packages/site/src/components/Header/Chain.tsx
+++ packages/site/src/components/Header/Chain.tsx
@@ -1,10 +1,21 @@
+import { useSwitchChain } from 'wagmi';
 import { useWrongChain } from '../../hooks';
 import { ConnectedDetails } from './styles';
+import { CHAIN_ID } from '../../config/wagmi';
 
 export const Chain = () => {
+  const { switchChain } = useSwitchChain();
   const { wrongChain, account } = useWrongChain();
+  
+  const handleSwitchChain = () => {
+    switchChain({ chainId: CHAIN_ID });
+  };
+  
   return (
-    <ConnectedDetails $wrongChain={wrongChain}>
+    <ConnectedDetails 
+      $wrongChain={wrongChain}
+      onClick={wrongChain ? handleSwitchChain : undefined}
+    >
       {wrongChain ? 'Wrong chain' : account.chain?.name}
     </ConnectedDetails>
   );
