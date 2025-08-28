cebb8dd5bd362e6e503139ef1d8b7cfa339f37c9
RafaBlockDev <rafafuentesrangel@outlook.com>
2025-08-28 11:50:53 -0600

    feat: set 2 environments in 2 networks

--- packages/site/src/config/snap.ts
+++ packages/site/src/config/snap.ts
@@ -1,26 +1,26 @@
-const isLocal = (): boolean => {
-    return import.meta.env.VITE_NODE_ENV === 'local';
+const isTestnet = (): boolean => {
+    return import.meta.env.VITE_NODE_ENV === 'testnet';
 };
 
 const isSnapLocal = (): boolean => {
   return import.meta.env.VITE_SNAP_ENV === 'local';
 };
 
-const isProduction = (): boolean => {
-    return import.meta.env.VITE_NODE_ENV === 'production';
+const isMainnet = (): boolean => {
+    return import.meta.env.VITE_NODE_ENV === 'mainnet';
 };
 
 const buildSnapOrigin = (): string => {
     if (isSnapLocal()) {
         return 'local:http://localhost:8080';
-    } else if (isProduction()) {
+    } else if (isMainnet()) {
         return 'npm:@coti-io/coti-snap';
     } else {
-        console.warn('VITE_NODE_ENV must be either "local" or "production"');
+        console.warn('VITE_NODE_ENV must be either "testnet" or "mainnet"');
         return 'npm:@coti-io/coti-snap';
     }
 };
 
 export const defaultSnapOrigin = buildSnapOrigin();
 
-export { isLocal, isProduction, buildSnapOrigin };
+export { isTestnet, isMainnet, buildSnapOrigin };
--- packages/site/src/config/wagmi.ts
+++ packages/site/src/config/wagmi.ts
@@ -23,12 +23,12 @@ export const CONNECTOR_MM: CONNECTOR = CONNECTOR_MM_REGULAR;
 export const CONNECTOR_MM_FLASK_EXPORT = CONNECTOR_MM_FLASK;
 export const CONNECTOR_MM_REGULAR_EXPORT = CONNECTOR_MM_REGULAR;
 
-const isLocal = import.meta.env.VITE_NODE_ENV === 'local';
+const isTestnet = import.meta.env.VITE_NODE_ENV === 'testnet';
 
 export const COTI_TESTNET_CHAIN_ID = 7082400;
 export const COTI_MAINNET_CHAIN_ID = 2632500;
 
-export const CHAIN_ID = isLocal ? COTI_TESTNET_CHAIN_ID : COTI_MAINNET_CHAIN_ID;
+export const CHAIN_ID = isTestnet ? COTI_TESTNET_CHAIN_ID : COTI_MAINNET_CHAIN_ID;
 
 const COTI_TESTNET = defineChain({
   id: COTI_TESTNET_CHAIN_ID,
@@ -74,7 +74,7 @@ const COTI_MAINNET = defineChain({
   contracts: {},
 });
 
-export const COTI = isLocal ? COTI_TESTNET : COTI_MAINNET;
+export const COTI = isTestnet ? COTI_TESTNET : COTI_MAINNET;
 
 export const config = createConfig({
   chains: [COTI],
