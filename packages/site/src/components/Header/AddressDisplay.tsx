bf447e66c6d7c375b1c34c6b91f1d3d55e77d5de
RafaBlockDev <rafafuentesrangel@outlook.com>
2025-10-06 08:41:03 -0600

    ui: arrows states with dropdown action

--- packages/site/src/components/Header/AddressDisplay.tsx
+++ packages/site/src/components/Header/AddressDisplay.tsx
@@ -3,6 +3,8 @@ import styled from 'styled-components';
 
 import { truncateString } from '../../utils';
 import { JazziconComponent } from '../common/JazziconComponent';
+import ArrowDownIcon from '../../assets/icons/arrowDown.svg';
+import UpArrowIcon from '../../assets/icons/up-arrow.svg';
 
 const AddressContainer = styled.div`
   display: flex;
@@ -22,16 +24,44 @@ const AddressText = styled.span`
   user-select: none;
 `;
 
+const ArrowIconWrapper = styled.div<{ $isOpen: boolean }>`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: relative;
+  width: 16px;
+  height: 16px;
+
+  svg {
+    position: absolute;
+    width: 16px;
+    height: 16px;
+    transition: all 200ms cubic-bezier(0.4, 0.0, 0.2, 1);
+  }
+
+  svg:first-child {
+    opacity: ${(props) => props.$isOpen ? '0' : '1'};
+    transform: ${(props) => props.$isOpen ? 'translateY(3px)' : 'translateY(0)'};
+  }
+
+  svg:last-child {
+    opacity: ${(props) => props.$isOpen ? '1' : '0'};
+    transform: ${(props) => props.$isOpen ? 'translateY(0)' : 'translateY(-3px)'};
+  }
+`;
+
 interface AddressDisplayProps {
   readonly address: string;
   readonly iconSize?: number;
   readonly truncate?: boolean;
+  readonly isOpen?: boolean;
 }
 
 export const AddressDisplay = memo<AddressDisplayProps>(({
   address,
   iconSize = 28,
-  truncate = true
+  truncate = true,
+  isOpen = false
 }) => {
   const displayAddress = truncate && address ? truncateString(address) : address || 'no address';
 
@@ -39,7 +69,10 @@ export const AddressDisplay = memo<AddressDisplayProps>(({
     <AddressContainer>
       <JazziconComponent address={address || ''} type="from" size={iconSize} />
       <AddressText>{displayAddress}</AddressText>
-      
+      <ArrowIconWrapper $isOpen={isOpen}>
+        <ArrowDownIcon />
+        <UpArrowIcon />
+      </ArrowIconWrapper>
     </AddressContainer>
   );
 });
