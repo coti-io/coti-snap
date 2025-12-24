25f459f8d1a5725131bb6f28c7d8f97976c4279f
RafaBlockDev <rafafuentesrangel@outlook.com>
2025-12-24 10:42:20 -0600

    fix: dropdown alignment

--- packages/site/src/router/Layout.tsx
+++ packages/site/src/router/Layout.tsx
@@ -3,22 +3,31 @@ import styled from 'styled-components';
 
 import { Header } from '../components';
 
+const LayoutWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+  width: 100%;
+  background: transparent;
+`;
+
 const Container = styled.div`
   display: flex;
   flex-direction: column;
   margin: auto;
   width: 564px;
   height: 100%;
-  max-height: 100vh;
-  margin-top: 20px;
-  max-height: calc(100vh - 120px);
+  margin-top: 80px;
+  padding-top: 24px;
   gap: 24px;
   box-sizing: border-box;
   background: transparent;
+
   ${({ theme }) => theme.mediaQueries.small} {
     width: 100%;
-    padding: 1.6rem;
-    margin: 0;
+    padding: 16px;
+    margin-top: 70px;
+    padding-top: 16px;
     max-width: 100vw;
     box-sizing: border-box;
   }
@@ -26,10 +35,12 @@ const Container = styled.div`
 
 export function Layout({ children }: { children?: React.ReactNode }) {
   return (
-    <Container>
+    <LayoutWrapper>
       <Header />
-      {children}
-      <Outlet />
-    </Container>
+      <Container>
+        {children}
+        <Outlet />
+      </Container>
+    </LayoutWrapper>
   );
 }
