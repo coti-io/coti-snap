383aee68e1358f0b843645d9542bde1a062ea9e2
RafaBlockDev <rafafuentesrangel@outlook.com>
2026-01-16 09:13:46 -0600

    fix: z-index

--- packages/site/src/components/Footer/index.tsx
+++ packages/site/src/components/Footer/index.tsx
@@ -15,7 +15,7 @@ const FooterContainer = styled.footer`
   height: 120px; /* Fixed height to prevent layout shift */
   box-sizing: border-box;
   contain: layout style;
-  z-index: 10;
+  z-index: -1;
 `;
 
 const Title = styled.h3`
